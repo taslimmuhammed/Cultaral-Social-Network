6b1b39eeb32372ea47c304d3e7c5477e4f53f936
taslimmuhammed <taslimmuhammed67@gmail.com>
2022-08-11 12:03:05 +0530

    admin-update

--- src/Comonents/Admin/Admin.js
+++ src/Comonents/Admin/Admin.js
@@ -14,16 +14,16 @@ function Admin() {
         setisLoading(true)
         try {
             await FInitiator()
-            //const v = await checkOwner()
-            // if (v != true) {
-            //     alert("Not Authorized")
-            //     navigate('/')
-            //     console.log("Hello")
-            // }else{
+            const v = await checkOwner()
+            if (v != true) {
+                alert("Not Authorized")
+                navigate('/')
+                console.log("Hello")
+            }else{
                 const datas = await getAdminDetails()
                 console.log(datas)
                 setData(datas)
-            //}
+            }
 
         } catch (e) {
             console.log(e)
@@ -70,7 +70,7 @@ function Admin() {
         <div> 
            {Data && <div>
             <div className='h_head'> Admin Panel</div>
-            <div className='h_box  text-white'>
+            <div className='h_box  text-white w-full text-center text-black'>
                 <div> Total number of tokens Supplied:</div>
                 <div className='text-green-400'>{Data.totalSupply}</div>
 
@@ -92,13 +92,11 @@ function Admin() {
 
                 <div className='mb-1'>Transfer ownership</div>
                 <input placeholder="new Address" className='text-black' onChange={(e) => { setAddress(e.target.value) }}></input>
-                <button className="button-8 mt-1" role="button" onClick={changeOwner1} >Change Owner</button>
+                <button className="button-24 mt-1" role="button" onClick={changeOwner1} >Change Owner</button>
                 <div>Change Unit max limit</div>
                 <input placeholder="new Limit" className='text-black' onChange={(e) => { setLimit(e.target.value) }} type="number"></input>
-                <button  className="button-8 mt-1" role="button" onClick={changeLimit1}>Change Limit</button>
+                <button  className="button-24 mt-1" role="button" onClick={changeLimit1}>Change Limit</button>
                 
-                <button className="button-24" role="button" onClick={withDr}>Withdraw Money</button>
-                {/* <button class="button-8" role="button" onClick={withDr}>WithDraw Balance USDT</button> */}
             </div>
         </div>}
     </div>
